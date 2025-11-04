d18bf62f8793a050be5c3bb6d17eb94c380314ac
denysa2004 <149471938+denysa2004@users.noreply.github.com>
2025-11-04 12:09:03 +0200

    Fix: make my-react-app a normal folder

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />}/> 
+      
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
