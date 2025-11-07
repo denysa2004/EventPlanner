e81f2bb60acd25b936851f994bd74eab4bcb057e
Vaidos Mihai <149139408+mihaivaidos@users.noreply.github.com>
2025-11-07 13:13:40 +0200

    Feat connect frontend with backend (#6)
    * Feat: Added default Home page.
    
    * Feat: Added connection with backend and handleRegister and handleLogin functions.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Home from "./pages/Home";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />}/> 
-      
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
